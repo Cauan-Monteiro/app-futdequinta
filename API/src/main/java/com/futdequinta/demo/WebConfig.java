4863c82d032b637148a2bcd7498581ca5abbfa4b
cauan-monteiro <cauansevero03@gmail.com>
2026-02-01 13:42:03 -0300

    tratando CORS para comunicação

---
+++ API/src/main/java/com/futdequinta/demo/WebConfig.java
@@ -0,0 +1,16 @@
+package com.futdequinta.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Libera todas as rotas da API
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Libera todos os verbos
+                .allowedOrigins("http://129.148.62.223"); // <--- AQUI É O PULO DO GATO 🐈
+    }
+}
