80e5599c37bcccb843d307a8a00c8724eb8db365
cauan-monteiro <cauansevero03@gmail.com>
2026-02-05 17:23:10 -0300

    Scrolling para partidas - 4

--- FutQuinta/src/App.tsx
+++ FutQuinta/src/App.tsx
@@ -498,7 +498,7 @@ function App() {
                 })
 
                 return (
-                  <div key={partida.id} className="bg-gray-800 rounded-lg shadow-lg p-6 min-w-full md:w-1/3 box-border shrink-0 scrollbar-hide snap-center">
+                  <div key={partida.id} className="bg-gray-800 rounded-lg shadow-lg p-6 min-w-full md:w-1/4 box-border shrink-0 scrollbar-hide snap-center">
                     <div className="mb-4">
                       <div className="flex justify-between items-center mb-2">
                         <span className="text-xs text-gray-400">{dataFormatada}</span>
